ac7f17d2b78bf96c7518f635d2b080d4242e70cd
Pat <chen.patrick47@gmail.com>
2021-10-29 12:23:00 +0200

    added bg

--- src/battle.php
+++ src/battle.php
@@ -24,11 +24,12 @@ if (isset($_POST['buttonSleep'])) {
     unset($_POST);
 }
 
-function uniquePost($posted) {
+function uniquePost($posted)
+{
     // take some form values
-    $description = $posted['t_betreff'].$posted['t_bereich'].$posted['t_nachricht'];
+    $description = $posted['t_betreff'] . $posted['t_bereich'] . $posted['t_nachricht'];
     // check if session hash matches current form hash
-    if (isset($_SESSION['form_hash']) && $_SESSION['form_hash'] == md5($description) ) {
+    if (isset($_SESSION['form_hash']) && $_SESSION['form_hash'] == md5($description)) {
         // form was re-submitted return false
         return false;
     }
@@ -40,50 +41,52 @@ function uniquePost($posted) {
 
 <!doctype html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Battle</title>
 </head>
+
 <body>
-<div class="battle-persos-container">
-    <?php
-    foreach ($allPersos as $key => $perso) {
+    <div class="battle-persos-container">
+        <?php
+        foreach ($allPersos as $key => $perso) {
         ?>
-        <div class="battle-perso-row">
-            <div class="battle-perso-element">
-                <?php echo $perso->getName() ?>
-            </div>
-            <div class="battle-perso-element">
-                <?php echo $perso->getType() ?>
-            </div>
-            <div class="battle-perso-element">
-                pv : <?php echo $perso->getPv() ?>
-            </div>
-            <div class="battle-perso-element">
-                force : <?php echo $perso->getForce() ?>
-            </div>
-            <div class="battle-perso-element">
-                defense : <?php echo $perso->getDefense() ?>
-            </div>
-            <div class="battle-perso-element">
-                <div class="battle-perso-buttons">
-                    <form method="post">
-                        <button type="post" name="buttonAttack" value="<?php echo $key ?>">
-                            Attaquer
-                        </button>
-                        <button type="post" name="buttonSleep" value="<?php echo $key ?>">
-                            Endormir
-                        </button>
-                    </form>
+            <div class="battle-perso-row">
+                <div class="battle-perso-element">
+                    <?php echo $perso->getName() ?>
+                </div>
+                <div class="battle-perso-element">
+                    <?php echo $perso->getType() ?>
+                </div>
+                <div class="battle-perso-element">
+                    pv : <?php echo $perso->getPv() ?>
+                </div>
+                <div class="battle-perso-element">
+                    force : <?php echo $perso->getForce() ?>
+                </div>
+                <div class="battle-perso-element">
+                    defense : <?php echo $perso->getDefense() ?>
+                </div>
+                <div class="battle-perso-element">
+                    <div class="battle-perso-buttons">
+                        <form method="post">
+                            <button type="post" name="buttonAttack" value="<?php echo $key ?>">
+                                Attaquer
+                            </button>
+                            <button type="post" name="buttonSleep" value="<?php echo $key ?>">
+                                Endormir
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php
-    }
-    ?>
-</div>
+        }
+        ?>
+    </div>
 </body>
+
 </html>
